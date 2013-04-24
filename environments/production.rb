aacbc0eb3f478a13561a66f5c5060b222ccb0503
Eric Saxby <sax@livinginthepast.org>
2013-04-23 22:46:04 -0700

    Add postgres-server role

---
+++ environments/production.rb
@@ -0,0 +1,16 @@
+name 'production'
+description 'Production environment'
+
+default_attributes(
+  'postgres' => {
+    'version' => '9.2.4',
+    'connections' => {
+      # Only trust IPs on your internal subnet
+      '127.0.0.1/0' => 'trust',
+      '10.0.0.0/8' => 'trust',
+    },
+    'replication' => {
+      '10.0.0.0/8' => 'trust',
+    }
+  }
+)
